41e9c202550672ef449439ef58655c6c048a8935
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2020-03-11 10:04:01 +0100

    added coroutines

--- app/src/main/java/com/example/android/bakingapp/network/BakingApiService.kt
+++ app/src/main/java/com/example/android/bakingapp/network/BakingApiService.kt
@@ -1,7 +1,9 @@
 package com.example.android.bakingapp.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -11,12 +13,14 @@ private const val BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/
 
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
-private val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi))
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL).build()
 
 interface BakingApiService {
     @GET("baking.json")
-    fun getProperties(): Call<List<BakingProperty>>
+    fun getProperties(): Deferred<List<BakingProperty>>
 }
 
 object BakingApi{
